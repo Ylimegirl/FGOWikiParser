b9fdc739ff050ba6b2c1d24f4fcaefd2ae6a1177
Ylimegirl <87622158+Ylimegirl@users.noreply.github.com>
2023-07-06 23:12:23 -0700

    v0.1.2
    i no longer know the difference between jpnodename and jpname and im too afraid to ask
    added support for parsing the entire war at once (albeit the war json doesn't include quest wave info or some other more specific stuff. it is what it is)
    added crystallized lore to item lookup
    added support for multiple battlefield types
    tbh i am this close :ok_hand: to learning how to deal with api in python to make this thing just work on its own lol

--- reader.py
+++ reader.py
@@ -3,6 +3,66 @@ from pretty import prettyJSON
 from items import itemLookup
 from mobs import mobLookup
 
+def parseQuest(quest):
+	new_file.write("{{Questheader\n|ap = " + str(quest["consume"]) + "\n")
+	new_file.write("|bond = ")
+	if "bond" in quest.keys():
+		new_file.write('{:,}'.format(quest["bond"]))
+	else:
+		new_file.write("0")
+	new_file.write("\n|qp = ")
+	if "qp" in quest.keys():
+		new_file.write('{:,}'.format(quest["qp"]))
+	else:
+		new_file.write("0")
+	new_file.write("\n|exp = ")
+	if "exp" in quest.keys():
+		new_file.write('{:,}'.format(quest["exp"]))
+	else:
+		new_file.write("0")
+	new_file.write("\n|image = " + "\n")
+	new_file.write("|jpname = " + quest["name"] + "\n")
+	new_file.write("|enname = " + "\n")
+	new_file.write("|type =")
+	if "individuality" in quest.keys():
+		for indiv in quest["individuality"]:
+			if(indiv["name"] != "unknown"):
+				new_file.write(" {{Battlefield Type|" + indiv["name"][5:].lower() + "}}")
+	else:
+		new_file.write(" ")
+	new_file.write("\n}}\n")
+	
+	if "phasesWithEnemies" in quest.keys() and len(quest["phasesWithEnemies"]) != 0 and not "stages" in quest.keys():
+		new_file.write("<center>'''MISSING QUEST DATA'''<br />(Parse the [https://api.atlasacademy.io/nice/JP/quest/" + str(quest["id"]) + " quest's JSON file] for this information!)</center>\n")
+	elif "stages" in quest.keys():
+		new_file.write("{{Questbody\n")
+		for wave in quest["stages"]:
+			currWave = str(wave["wave"])
+			new_file.write("|battle" + currWave + " = Battle " + currWave + "/" + str(len(quest["stages"])) + "\n")
+			for enemy in wave["enemies"]:
+				new_file.write("|en" + currWave + str(enemy["deckId"]) + " = [[" +mobLookup(enemy["svt"]["name"], enemy["svt"]["collectionNo"]) + "|" + enemy["name"] + "]];")
+				new_file.write(" Lvl " + str(enemy["lv"]) + " {{" + enemy["svt"]["className"].capitalize())
+				if(enemy["svt"]["rarity"] == 1 or enemy["svt"]["rarity"] == 2):
+					new_file.write("Bronze")
+				elif(enemy["svt"]["rarity"] == 3):
+					new_file.write("Silver")
+				new_file.write("}} " + '{:,}'.format(enemy["hp"]) + " HP\n")
+		
+		new_file.write("|dropicons =")
+		for group in quest["drops"]:
+			new_file.write(" " + itemLookup(group["objectId"], group["type"], group["num"]))
+		new_file.write("\n")
+		new_file.write("}}\n")
+	else:
+		new_file.write("<center>'''NO BATTLE'''</center>\n")
+	
+	new_file.write("{{Questfooter\n")
+	new_file.write("|reward =")
+	for group in quest["gifts"]:
+		new_file.write(" " + itemLookup(group["objectId"], group["type"], group["num"]))
+	new_file.write("\n")
+	new_file.write("}}\n")
+
 if not os.path.exists("inputs"):
 	os.mkdir("inputs")
 if not os.path.exists("outputs"):
@@ -35,43 +95,14 @@ for item in files:
 			os.remove(newname)
 		new_file = open(newname, "a", encoding="utf-8")
 		
-		new_file.write("{{Questheader\n|ap = " + str(dict["consume"]) + "\n")
-		new_file.write("|bond = " + '{:,}'.format(dict["bond"]) + "\n")
-		new_file.write("|qp = " + '{:,}'.format(dict["qp"]) + "\n")
-		new_file.write("|exp = " + '{:,}'.format(dict["exp"]) + "\n")
-		new_file.write("|image = " + "\n")
-		new_file.write("|jpnodename = " + dict["name"] + "\n")
-		new_file.write("|ennodename = " + "\n")
-		new_file.write("|jpname = " + "\n")
-		new_file.write("|enname = " + dict["name"] + "\n")
-		new_file.write("|type = {{Battlefield Type|" + dict["individuality"][0]["name"][5:].lower() + "}}\n")
-		new_file.write("}}\n")
-		
-		new_file.write("{{Questbody\n")
-		for wave in dict["stages"]:
-			currWave = str(wave["wave"])
-			new_file.write("|battle" + currWave + " = Battle " + currWave + "/" + str(len(dict["stages"])) + "\n")
-			for enemy in wave["enemies"]:
-				new_file.write("|en" + currWave + str(enemy["deckId"]) + " = [[" +mobLookup(enemy["svt"]["name"], enemy["svt"]["collectionNo"]) + "|" + enemy["name"] + "]];")
-				new_file.write(" Lvl " + str(enemy["lv"]) + " {{" + enemy["svt"]["className"].capitalize())
-				if(enemy["svt"]["rarity"] == 1 or enemy["svt"]["rarity"] == 2):
-					new_file.write("Bronze")
-				elif(enemy["svt"]["rarity"] == 3):
-					new_file.write("Silver")
-				new_file.write("}} " + '{:,}'.format(enemy["hp"]) + " HP\n")
-		
-		new_file.write("|dropicons =")
-		for group in dict["drops"]:
-			new_file.write(" " + itemLookup(group["objectId"], group["type"], group["num"]))
-		new_file.write("\n")
-		new_file.write("}}\n")
-		
-		new_file.write("{{Questfooter\n")
-		new_file.write("|reward =")
-		for group in dict["gifts"]:
-			new_file.write(" " + itemLookup(group["objectId"], group["type"], group["num"]))
-		new_file.write("\n")
-		new_file.write("}}\n")
+		if "spots" in dict.keys():
+			for spot in dict["spots"]:
+				for quest in spot["quests"]:
+					parseQuest(quest)
+					new_file.write("\n")
+					
+		else:
+			parseQuest(dict)
 
 		new_file.write("---------------------------------------------\nFGO Wiki Parser v" + verNum + " by Ylimegirl\nhttps://github.com/Ylimegirl")
 		new_file.close()
